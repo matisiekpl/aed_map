c9c857bd6026ce7e9628ed0d28ae0e8a00f584a2
Mateusz Woźniak <matisiek11@gmail.com>
2022-08-05 00:12:11 +0300

    :zap: Added user heading pointer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:maps_launcher/maps_launcher.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:vector_map_tiles/vector_map_tiles.dart';
 import 'package:vector_tile_renderer/vector_tile_renderer.dart';
 
@@ -22,6 +25,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<AED> aeds = [];
+  AED? selectedAED;
 
   @override
   void initState() {
@@ -33,6 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
     var _aeds = await Store.instance.loadAEDs();
     setState(() {
       aeds = _aeds;
+      selectedAED = aeds[0];
     });
   }
 
@@ -49,10 +54,12 @@ class _HomeScreenState extends State<HomeScreen> {
     return CupertinoPageScaffold(
         child: aeds.isEmpty
             ? Center(child: CircularProgressIndicator())
-            : SlidingUpPanel(borderRadius: radius, panel: Container(decoration: BoxDecoration(borderRadius: radius), child: _buildBottomPanel()), body: SafeArea(child: _buildMap(), top: false)));
+            : SlidingUpPanel(
+                maxHeight: 400, borderRadius: radius, panel: Container(decoration: BoxDecoration(borderRadius: radius), child: _buildBottomPanel()), body: SafeArea(child: _buildMap(), top: false)));
   }
 
   Widget _buildBottomPanel() {
+    var aed = selectedAED!;
     return Column(children: [
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -65,39 +72,178 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
+      Padding(
+        padding: const EdgeInsets.only(left: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('⚠️ Najbliższy AED', style: TextStyle(color: Colors.orange, fontStyle: FontStyle.italic, fontSize: 18)),
+              ],
+            ),
+            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [Text('🫀 Defibrylator AED', style: TextStyle(fontSize: 24))],
+            ),
+            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('Dokładna lokalizacja: ', style: TextStyle(fontSize: 16)),
+                Text(aed.description ?? 'brak danych', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('Wewnątrz budynku: ', style: TextStyle(fontSize: 16)),
+                Text(aed.indoor ? 'tak' : 'nie', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('Operator: ', style: TextStyle(fontSize: 16)),
+                Text(aed.operator ?? 'brak danych', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 4),
+            GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              onTap: () {
+                print('Calling to: tel:${aed.phone.toString().replaceAll(' ', '')}');
+                launchUrl(Uri.parse('tel:${aed.phone.toString().replaceAll(' ', '')}'));
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('Kontakt: ', style: TextStyle(fontSize: 16)),
+                  Text(aed.phone ?? 'brak danych', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            SizedBox(height: 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('Identyfikator: ', style: TextStyle(fontSize: 16)),
+                Text(aed.id.toString(), style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Builder(builder: (BuildContext context) {
+                    var message = 'Szer: ${aed.location.latitude}';
+                    return StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+                      return TextButton(
+                          child: Text(message, style: TextStyle(fontSize: 16, color: Colors.black)),
+                          onPressed: () {
+                            Clipboard.setData(ClipboardData(text: aed.location.latitude.toString()));
+                            setState(() {
+                              message = 'Skopiowano';
+                            });
+                            Future.delayed(const Duration(milliseconds: 2000), () {
+                              setState(() {
+                                message = 'Dł: ${aed.location.latitude}';
+                              });
+                            });
+                          });
+                    });
+                  }),
+                  Builder(builder: (BuildContext context) {
+                    var message = 'Dł: ${aed.location.longitude}';
+                    return StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+                      return TextButton(
+                          child: Text(message, style: TextStyle(fontSize: 16, color: Colors.black)),
+                          onPressed: () {
+                            Clipboard.setData(ClipboardData(text: aed.location.longitude.toString()));
+                            setState(() {
+                              message = 'Skopiowano';
+                            });
+                            Future.delayed(const Duration(milliseconds: 2000), () {
+                              setState(() {
+                                message = 'Dł: ${aed.location.longitude}';
+                              });
+                            });
+                          });
+                    });
+                  }),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: SizedBox(
+                  width: double.infinity,
+                  child: CupertinoButton.filled(
+                      child: Text('Nawiguj'),
+                      onPressed: () {
+                        MapsLauncher.launchCoordinates(aed.location.latitude, aed.location.longitude);
+                      })),
+            ),
+            SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('~9 minut biegiem (450m)', style: TextStyle(color: Colors.orange)),
+              ],
+            )
+          ],
+        ),
+      )
     ]);
   }
 
   Widget _buildMap() {
-    // return FutureBuilder<Position>(
-    //     future: Store.instance.determinePosition(),
-    //     builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
-    //       if (snapshot.hasData || snapshot.hasError) {
-    return Column(
-      children: [
-        Flexible(
-            child: Stack(
-          children: [
-            FlutterMap(
-              // options: MapOptions(center: snapshot.hasData ? LatLng(snapshot.data!.latitude, snapshot.data!.longitude) : warsaw, zoom: 14, maxZoom: 18, plugins: [VectorMapTilesPlugin()]),
-              options: MapOptions(center: warsaw, zoom: 14, maxZoom: 18, plugins: [VectorMapTilesPlugin()]),
-              layers: <LayerOptions>[
-                VectorTileLayerOptions(theme: _getMapTheme(context), tileProviders: TileProviders({'openmaptiles': Store.instance.buildCachingTileProvider()})),
-                MarkerLayerOptions(markers: _getMarkers())
+    return FutureBuilder<Position>(
+        future: Store.instance.determinePosition(),
+        builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+          if (snapshot.hasData || snapshot.hasError) {
+            return Column(
+              children: [
+                Flexible(
+                    child: Stack(
+                  children: [
+                    FlutterMap(
+                      options: MapOptions(
+                          center: snapshot.hasData ? LatLng(snapshot.data!.latitude, snapshot.data!.longitude) : warsaw,
+                          zoom: 14,
+                          maxZoom: 18,
+                          plugins: [VectorMapTilesPlugin(), LocationMarkerPlugin()]),
+                      // options: MapOptions(center: warsaw, zoom: 14, maxZoom: 18, plugins: [VectorMapTilesPlugin()]),
+                      layers: <LayerOptions>[
+                        VectorTileLayerOptions(theme: _getMapTheme(context), tileProviders: TileProviders({'openmaptiles': Store.instance.buildCachingTileProvider()})),
+                        MarkerLayerOptions(markers: _getMarkers()),
+                        LocationMarkerLayerOptions(),
+                      ],
+                    ),
+                    SafeArea(
+                        child: Padding(
+                      padding: const EdgeInsets.only(left: 16, top: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Mapa AED', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32)),
+                          Text('${aeds.length} AED dostępnych', style: TextStyle(fontSize: 14))
+                        ],
+                      ),
+                    )),
+                  ],
+                )),
               ],
-              children: [LocationMarkerLayerWidget()],
-            ),
-            SafeArea(child: Padding(
-              padding: const EdgeInsets.only(left: 16, top: 8),
-              child: Text('Mapa AED', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32)),
-            )),
-          ],
-        )),
-      ],
-    );
-    // }
-    // return const Center(child: CircularProgressIndicator());
-    // });
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        });
   }
 
   _getMapTheme(BuildContext context) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import geolocator_apple
+import maps_launcher
 import path_provider_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  MapsLauncherPlugin.register(with: registry.registrar(forPlugin: "MapsLauncherPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
