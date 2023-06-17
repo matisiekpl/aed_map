763363316d2d73cc38805d7de3348cabc1ac2773
Mateusz Woźniak <matisiek11@gmail.com>
2023-06-17 22:14:39 +0200

    Disabled impeller

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 start=$(date +%s)
 ./setup.sh
-echo "Running integration tests 🧪"
-flutter drive \
-  --profile --cache-sksl --write-sksl-on-exit flutter_01.sksl.json \
-  --driver=test_driver/integration_test.dart \
-  --target=integration_test/app_test.dart &>/dev/null
+#echo "Running integration tests 🧪"
+#flutter drive \
+#  --profile --cache-sksl --write-sksl-on-exit flutter_01.sksl.json \
+#  --driver=test_driver/integration_test.dart \
+#  --target=integration_test/app_test.dart &>/dev/null
 echo "Building archives 🏗️"
-flutter build ipa --bundle-sksl-path flutter_01.sksl.json &>/dev/null
-flutter build appbundle --bundle-sksl-path flutter_01.sksl.json &>/dev/null
+flutter build ipa &>/dev/null
+flutter build appbundle &>/dev/null
 end=$(date +%s)
 echo "Done! 🚀 ($(($end-$start)) s)"
