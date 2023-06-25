37be35b0c0c1211e8ce4951b05818cad018197fe
Mateusz Woźniak <matisiek11@gmail.com>
2023-06-25 20:05:48 +0200

    Added github actions pipeline

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,6 @@
 import 'package:latlong2/latlong.dart';
 
 final warsaw = LatLng(52.237049, 21.017532);
-final rzeszow = LatLng(50.041187, 21.999121);
 const valhalla = 'https://routing.aedmapa.pl';
 const plausible = 'https://analytics.chatspell.co';
 const iosUserAgent =
