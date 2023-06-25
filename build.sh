e9e817035fc63a24ce751763255cdaae3d353f66
Mateusz Woźniak <matisiek11@gmail.com>
2023-06-25 20:16:45 +0200

    Fixed AEDs count on landing page

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 
 start=$(date +%s)
 ./setup.sh
-#echo "Running integration tests 🧪"
-#flutter drive \
-#  --profile --cache-sksl --write-sksl-on-exit flutter_01.sksl.json \
-#  --driver=test_driver/integration_test.dart \
-#  --target=integration_test/app_test.dart &>/dev/null
 echo "Building archives 🏗️"
 flutter build ipa
 flutter build appbundle
