db5cc6c31eb2d8cd0b2059aad7eaffe7175b886e
Mateusz Woźniak <matisiek11@gmail.com>
2025-07-03 11:59:09 +0200

    fix: linting errors

--- lib/screens/map/map_header.dart
+++ lib/screens/map/map_header.dart
@@ -12,7 +12,6 @@ import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mixpanel_flutter/mixpanel_flutter.dart';
 import 'package:solidchat/solidchat.dart';
 
 import '../../bloc/panel/panel_cubit.dart';
