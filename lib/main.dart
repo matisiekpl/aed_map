2b98d1c0e4e7218463fd46f2b9600f37c4f53067
Mateusz Woźniak <matisiek11@gmail.com>
2026-04-15 18:10:27 +0200

    chore: update data

--- lib/main.dart
+++ lib/main.dart
@@ -57,8 +57,8 @@ void main() async {
           'https://492fa94bb5e0bdf492c5a8b8a108d84e@o337011.ingest.sentry.io/4506661810274304';
       options.tracesSampleRate = 1.0;
       options.attachScreenshot = true;
-      options.experimental.replay.sessionSampleRate = 1.0;
-      options.experimental.replay.onErrorSampleRate = 1.0;
+      options.replay.sessionSampleRate = 1.0;
+      options.replay.onErrorSampleRate = 1.0;
     },
     appRunner: () => runApp(
         SentryWidget(child: BetterFeedback(child: Phoenix(child: App())))),
